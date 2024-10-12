1fbc7ba5e21947e6dec9e942feae230ea84e39dd
HawaKake <hawakakey10@gmail.com>
2024-10-12 23:24:47 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+
+//module.exports = {
+ // content: [],
+  //theme: {
+  //  extend: {},
+  //},
+  //plugins: [],
+//}
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
